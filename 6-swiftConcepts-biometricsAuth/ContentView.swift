23c1c8658ce5dd4a3d6f73478a29b9a607b77f16
Liu Ziyi <ziyi.liu02@gmail.com>
2023-09-06 15:57:51 +0800

    - Implement authenticate method

--- 6-swiftConcepts-biometricsAuth/ContentView.swift
+++ 6-swiftConcepts-biometricsAuth/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Liu Ziyi on 6/9/23.
 //
 
+import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
@@ -16,6 +17,26 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    // authenticated successfully
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics 
+        }
+        
     }
 }
 
