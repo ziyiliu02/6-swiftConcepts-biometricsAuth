0f0479d7904b15e38f4c140b87085e63f452c1c0
Liu Ziyi <ziyi.liu02@gmail.com>
2023-09-06 14:28:42 +0800

    Initial Commit

---
+++ 6-swiftConcepts-biometricsAuth/__swiftConcepts_biometricsAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  __swiftConcepts_biometricsAuthApp.swift
+//  6-swiftConcepts-biometricsAuth
+//
+//  Created by Liu Ziyi on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct __swiftConcepts_biometricsAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
